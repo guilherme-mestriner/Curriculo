2e8f73863782d82b54e07a7e81ac61e57ddad432
Guilherme Mestriner <gm5842039@gmail.com>
2023-04-14 21:58:47 -0300

    Curriculo

---
+++ src/pages/Curriculo.js
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const Home = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Pagina Inicial</Text>
+
+            <Button 
+                title="Formação"
+                onPress={ () => navigation.navigate('Formacao') }
+            />
+        </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+
+});
+
+export default Curriculo;

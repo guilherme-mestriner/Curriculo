31da849c938f24bd8afde468d91f37219ddf52de
Guilherme Mestriner <gm5842039@gmail.com>
2023-04-19 22:36:56 -0300

    NovoDesign

--- src/pages/Curriculo.js
+++ src/pages/Curriculo.js
@@ -1,26 +1,71 @@
-import { React } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+import Botao from "../components/Botao";
 
 const Curriculo = ({ navigation }) => {
     return (
-        <View>
-            <Text>Meu Curriculo</Text>
+        <View style={estilo.container}>
+            <View style={estilo.imagemContainer}>
+                <Image source={require('../images/eu.jpg')} style={{ width: 175, height: 175, borderRadius: 100 }} />
+            </View>
 
-            <Button 
-                title="Sobre mim"
-                onPress={ () => navigation.navigate('Formacao') }
-            />
+            <View style={estilo.textoContainer}>
+                <Text style={[estilo.texto, estilo.destaque]}>Olá,</Text>
 
-            <Button 
-                title="Objetivo"
-                onPress={ () => navigation.navigate('Objetivo') }
-            />
+                <Text style={estilo.texto}>
+                    meu nome é <Text style={estilo.destaque}>Guilherme Mestriner</Text> e este é o meu currículo.
+                </Text>
+            </View>
+
+            <View style={estilo.botoes}>
+                <Botao titulo="Curriculo" acao={ () => navigation.navigate('Curriculo') } />
+                <Botao titulo="Formação" acao={ () => navigation.navigate('Formacao') } />
+                <Botao titulo="Objetivo" acao={ () => navigation.navigate('Objetivo') } />
+                <Botao titulo="Experiências" acao={ () => navigation.navigate('Experiencia') } />
+            </View>
         </View>
     );
 }
 
-const estilos = StyleSheet.create({
-
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        padding: 10,
+        fontFamily: "Trebuchet MS",
+    },
+    textoContainer: {
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 2,
+    },
+    imagemContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexGrow: 1,
+    },
+    texto: {
+        fontSize: "3em",
+        fontFamily: 'inherit',
+    },
+    destaque: {
+        color: "#00a",
+        fontWeight: "bold",
+    },
+    botoes: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "row",
+        flexGrow: 2,
+        justifyContent: "space-between",
+        padding: 10,
+        backgroundColor: "#00a",
+        borderRadius: 3,
+        gap: 1,
+    },
 });
 
 export default Curriculo;
